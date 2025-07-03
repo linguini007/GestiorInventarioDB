dabd3126a3773f9c032c521e7ed172ea79d04c34
SET-DET <69767069+linguini007@users.noreply.github.com>
2025-07-02 21:06:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#main
+import Front
+
+
+class Main_tienda(): #Funcion MAIN  llamada Main_tienda() 
+    def __init__(self): #Que contiene como inicializacion una funcion que manda
+        Front.LoginApp()      #a llamar a la funcion LoginApp() que es nuestro login al inicio del programa
+                    #Dentro de esa funcionesta la llamada a la funcion consultas que es el resto del programa
+Main_tienda() #INICIAMOS EL PROGRAMA
